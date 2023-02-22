38f4a2fc0a2ed921659c536cfef2a23eb54a1d04
Ernesto <ernesto931117@gmail.com>
2023-02-22 01:58:39 -0500

    Add endpoints for favorites series

--- app/Http/Controllers/SerieController.php
+++ app/Http/Controllers/SerieController.php
@@ -28,12 +28,43 @@ class SerieController extends Controller
     public function show($id)
     {
         $serieR = Serie::with(['seasons.chapters', 'seasons.subtitles', 'images'])->find($id);
+        $serieR->views++;
+        $serieR->save();
         return response()->json($serieR);
+    }
+
+    public function increaseDownload($id){
+        $season = Season::find($id);
+        $serie = Serie::find($season->serie_id);
+        $serie->downloads++;
+        $serie->save();
+        return response()->json($serie);
     }
 
     public function searchByName($term)
     {
         $serieR = Serie::with(['seasons.chapters', 'seasons.subtitles', 'images'])->where('title', 'LIKE', '%' . $term . '%')->get();
         return response()->json($serieR);
+    }
+
+    public function popularSeries(){
+        $popularSeries = Serie::with(['seasons.chapters', 'seasons.subtitles', 'images'])->orderBy('views','DESC')->limit(10)->get();
+        return response()->json($popularSeries);
+    }
+
+    public function mostPopular(){
+        $popularSeries = Serie::orderBy('views','DESC')->limit(1)->get();
+        $mostPopular = Serie::with(['seasons.chapters', 'seasons.subtitles', 'images'])->find($popularSeries[0]->id);
+        return response()->json($mostPopular);
+    }
+
+    public function mostDownloads(){
+        $popularSeries = Serie::with(['seasons.chapters', 'seasons.subtitles', 'images'])->orderBy('downloads','DESC')->limit(10)->get();
+        return response()->json($popularSeries);
+    }
+
+    public function randomSeries(){
+        $randomSeries = Serie::with(['seasons.chapters', 'seasons.subtitles', 'images'])->inRandomOrder()->limit(10)->get();
+        return response()->json($randomSeries);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,16 +20,26 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('series',[SerieController::class,"index"])->name('series');
+Route::get('series', [SerieController::class, "index"])->name('series');
 
-Route::get('series/{id}',[SerieController::class,"show"])->name('showSerie');
+Route::get('series/most_download', [SerieController::class, "mostDownloads"])->name('mostDownloads');
 
-Route::get('series/search/{name}',[SerieController::class,"searchByName"])->name('searchByName');
+Route::get('series/popular', [SerieController::class, "popularSeries"])->name('popularSeries');
+
+Route::get('series/random', [SerieController::class, "randomSeries"])->name('randomSeries');
+
+Route::get('series/best', [SerieController::class, "mostPopular"])->name('mostPopular');
+
+Route::get('series/download/{id}', [SerieController::class, 'increaseDownload'])->name('increaseDownload');
+
+Route::get('series/{id}', [SerieController::class, "show"])->name('showSerie');
+
+Route::get('series/search/{name}', [SerieController::class, "searchByName"])->name('searchByName');
 
 Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'login']);
 
-Route::group( ['middleware' => ["auth:sanctum"]], function(){
+Route::group(['middleware' => ["auth:sanctum"]], function () {
     //rutas
     Route::get('user-profile', [UserController::class, 'userProfile']);
     Route::get('logout', [UserController::class, 'logout']);
